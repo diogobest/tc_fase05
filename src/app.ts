66450e33580b8ad49039a7a1e5a310cd0b19ae29
Diogo <dev-02@sscrop.com>
2026-09-09 13:40:10 -0300

    start project

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+const port = Number(process.env.PORT ?? 3000);
+
+app.listen(port);
